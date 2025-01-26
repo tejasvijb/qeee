83c4b007aabc04f7ab92ff8c5dd84943a0ce1dc1
tejasvijb <jbtejasvi@gmail.com>
2025-01-26 18:05:55 +0530

    add favicon

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import DragAndDropUpload from "@/components/custom/FileUpload";
 export default function Home() {
     return (
         <div className="h-screen grid grid-cols-3">
+
             <div className="col-span-2 max-sm:col-span-3 bg-[rgb(170,198,255)] p-10 max-sm:p-4">
                 <div className="bg-[#CCDDFF] p-10 max-sm:p-4 h-full rounded-3xl">
                     <DragAndDropUpload />
